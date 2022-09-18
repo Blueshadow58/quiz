0acb67e90e0c629da7217b1c13a6ab1b95ea4195
Blueshadow58 <gamonalfranco@gmail.com>
2022-09-18 19:32:38 -0300

    cleaning console warnings

--- src/Shuffled.jsx
+++ src/Shuffled.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Shuffled = (unshuffled) => {
   let shuffled = unshuffled
     .map((value) => ({ value, sort: Math.random() }))
--- src/Token.jsx
+++
@@ -1,8 +0,0 @@
-import md5 from "md5-hash";
-
-const Token = (data) => {
-  const hash = md5(data);
-  return hash;
-};
-
-export default Token;
